4d6c48fb06bc35b06f5237e3d973bcd1de67aceb
Ihury Prado <ihurycprado@gmail.com>
2025-02-13 20:59:09 -0300

    Atualização do dia 13/02/2025 e de mais alguns dias atrás.

--- djangoapp/utils/images.py
+++ djangoapp/utils/images.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+
 from django.conf import settings
 from PIL import Image
+
 
 def resize_image(image_django, new_width=800, optimize=True, quality=60):
     image_path = Path(settings.MEDIA_ROOT / image_django.name).resolve()
@@ -14,10 +16,11 @@ def resize_image(image_django, new_width=800, optimize=True, quality=60):
     new_height = round(new_width * original_height / original_width)
 
     new_image = image_pillow.resize((new_width, new_height), Image.LANCZOS)
-    
+
     new_image.save(
         image_path,
         optimize=optimize,
         quality=quality,
     )
+
     return new_image
